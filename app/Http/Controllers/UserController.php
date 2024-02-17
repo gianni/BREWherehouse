014c6cd16b8986c9227384f40ba3fa5a51f936b3
gianni <fiorentinig@gmail.com>
2024-02-17 23:03:06 +0000

    refactor: removed unused packages

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
