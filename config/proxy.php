0f7e8bea518dac58c89ea84e7d5674da9b2f6fb6
gianni <fiorentinig@gmail.com>
2024-02-17 11:22:18 +0000

    fix: linting

--- config/proxy.php
+++ config/proxy.php
@@ -2,6 +2,6 @@
 
 return [
     'endpoint' => env('PROXY_ENDPOINT', null),
-    'items_total'=> env('PROXY_ITEMS_TOTAL', null),
-    'items_per_page'=> env('PROXY_ITEMS_PER_PAGE', 15),
+    'items_total' => env('PROXY_ITEMS_TOTAL', null),
+    'items_per_page' => env('PROXY_ITEMS_PER_PAGE', 15),
 ];
